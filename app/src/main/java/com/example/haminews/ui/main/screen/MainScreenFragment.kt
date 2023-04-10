b2cc9d150c8772710d951b1d3802f4d9de55af32
Khamadekh Rami <khamadekh@gmail.com>
2023-04-10 21:34:07 +0700

    add init adapter in MainScreenFragment.kt + add class GetNews in utils

--- app/src/main/java/com/example/haminews/ui/main/screen/MainScreenFragment.kt
+++ app/src/main/java/com/example/haminews/ui/main/screen/MainScreenFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.haminews.adapters.CardNewsAdapter
 import com.example.haminews.databinding.FragmentMainScreenBinding
+import com.example.haminews.utils.GetNews
 
 class MainScreenFragment : Fragment() {
+    private lateinit var adapter: CardNewsAdapter
     private var _binding: FragmentMainScreenBinding? = null
     private val binding get() = _binding!!
 
@@ -18,6 +22,30 @@ class MainScreenFragment : Fragment() {
         //Раздуваем макет для фрагмента
         _binding = FragmentMainScreenBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        adapter = CardNewsAdapter {
+
+        }
+        binding.rvItemCardNews.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvItemCardNews.adapter = adapter
+
+        adapter.submitList(
+            GetNews.getListNews()
+        )
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
